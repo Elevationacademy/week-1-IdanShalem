b8667e6767a4287aa5dba563a38cfc19a777a470
Idan Shalem <idanshalem25@gmail.com>
2020-08-13 13:26:41 +0300

    excercise 2

---
+++ Day-5/Reservations/main.js
@@ -0,0 +1,38 @@
+const reservations = {
+    Bob: { claimed: false },
+    Ted: { claimed: true }
+  }
+  
+  const clicked = document.getElementById("submit-res")
+  const messageBody = document.getElementById("message")
+
+  clicked.onclick = function() {
+    let name = document.getElementById("res-name").value
+    name = name.charAt(0).toUpperCase() + name.slice(1)
+    checkReservation(name)
+    name.value = ''
+  }
+
+const messager = function(msg, name) {
+    if(msg === 1) {
+        messageBody.innerHTML = `Hmm, ${name} someone already claimed this reservation`
+    } else if (msg === 2) {
+        messageBody.innerHTML = 'Welcome ' + name
+    } else {
+        messageBody.innerHTML = `${name}, you have no reservation, I will add it now`
+    }
+}
+
+const checkReservation = function(name){ 
+    if (reservations[name] != undefined) {
+        if (reservations[name].claimed) {      
+            messager(1, name)
+        } else {
+            reservations[name].claimed = true
+            messager(2, name)
+        }
+    } else {
+        messager(3, name)
+        reservations[name] = {claimed : true}
+    }
+}
